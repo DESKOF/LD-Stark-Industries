72ac604606ba37b853fc5148148e51fb607de678
Feffie <44549940+Feffie@users.noreply.github.com>
2018-11-11 12:53:49 -0600

    Update WebImplementation.v

--- WebImplementation.v
+++ WebImplementation.v
@@ -3,7 +3,7 @@
 // Decoder to take the user's choice and determine which bit to output a one on.
 // The select[0] is the lsb, while the select[2] is the msb. The out[0]is the lsb,
 // and the out[7] is the msb. Output out will be one-hot.
-module Decoder(input [2:0] select, enable, output [7:0] out);
+module Decoder(input [2:0] select, input enable, output [7:0] out);
 	reg [7:0] out_temp;
 	
 	always @*
@@ -27,21 +27,22 @@ endmodule
 //==================================================================================
 // This module interprets the output of the Decoder module and determines what kind
 // of web that the user wants to use.
-module DecChoice(input [7:0] energy, input [5:0] tracers, input [3:0] fluid, input [2:0] select, enable);
+module DecChoice(input [7:0] energy, input [5:0] tracers, input [3:0] fluidtemp, input [2:0] select, input enable, clk);
 	wire [7:0] choice; // will hold the one-hot binary bit string from the decoder
+	wire [3:0] fluid;
 	
 	Decoder dec (select, enable, choice); //determine the web that the user wants
 	
-	always @*
+	always @(select)
 		case (choice)
-			8'b10000000: ;// call swing line web resource checker
-			8'b01000000: ;// call ricochet web resource checker
-			8'b00100000: ;// call splitter web resource checker
-			8'b00010000: ;// call web grenade resource checker
-			8'b00001000: ;// call taser web resource checker
-			8'b00000100: ;// call rapid fire web resource checker
-			8'b00000010: ;// call spider-tracer web resource checker
-			8'b00000001: ;// call reload fluid cartridge
+			8'b10000000: ; // call swing line web resource checker
+			8'b01000000: ; // call ricochet web resource checker
+			8'b00100000: ; // call splitter web resource checker
+			8'b00010000: ; // call web grenade resource checker
+			8'b00001000: ; // call taser web resource checker
+			8'b00000100: ; // call rapid fire web resource checker
+			8'b00000010: ; // call spider-tracer web resource checker
+			8'b00000001: ; // call reload fluid cartridge
 			default: $display("You did something wrong");
 		endcase
 	
