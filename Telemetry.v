2d633d2ec6cb2ff6bd004d025fa6711e53c0a535
amedrano5 <38707411+amedrano5@users.noreply.github.com>
2018-11-14 21:24:21 -0600

    Add files via upload

--- Telemetry.v
+++ Telemetry.v
@@ -1,17 +1,54 @@
-
-module DFF(clk, D, Q);
-	parameter n=1;
+//DFFs hold single bits for each coordinate
+module DFF(rst, clk, D, Q);
 	input clk;
-	input [n-1:0] D;
-	output [n-1:0] Q;
-	reg [n-1:0] Q;
+	input D;
+	input rst;
+	output Q;
+	reg Q;
 
 	always @(posedge clk)
-	Q = D;
+	begin
+	if(rst)
+		Q = 0;
+	else 
+		Q = D;
+	end
 endmodule
 
+//determines the target for each
+module Decoder(in, out, enable);
+	input [7:0] in;
+	input enable;
+	output [31:0] out;
+	wire [31:0] out;
 
-module CoordX(clk, in, out);
+	always @ (in)
+	assign out = (enable) ? (1 << in): 32'b0;
+
+endmodule
+
+//selector for each dff
+module Mux(nochange, load, sel, out);
+	//In and Outs
+	input nochange;
+	input load;
+	input sel;
+	output out;
+
+	//Internal Vars
+	reg  out;
+	
+	always @ (sel)
+	begin
+ 	case(sel) 
+		1'b0 : out = nochange;
+		1'b1 : out = load;
+	default out = 1'bx;
+ 	endcase 
+end
+
+//Module that holds 8-bits for each coordinate and time
+module Coord(clk, in, out);
 	input [7:0] in;
 	output [7:0] out;
 	
@@ -24,41 +61,4 @@ module CoordX(clk, in, out);
 	DFF second(clk, in[1], out[1]);
 	DFF first(clk, in[0], out[0]);
 	
-endmodule
-
-module CoordY(clk, in, out);
-	input [7:0] in;
-	output [7:0] out;
-	
-	DFF eigth(clk, in[7], out[7]);
-	DFF seventh(clk, in[6], out[6]);
-	DFF sixth(clk, in[5], out[5]);
-	DFF fifth(clk, in[4], out[4]);
-	DFF fourth(clk, in[3], out[3]);
-	DFF third(clk, in[2], out[2]);
-	DFF second(clk, in[1], out[1]);
-	DFF first(clk, in[0], out[0]);
-	
-endmodule
-
-module CoordZ(clk, in, out);
-	input [7:0] in;
-	output [7:0] out;
-	
-	DFF eigth(clk, in[7], out[7]);
-	DFF seventh(clk, in[6], out[6]);
-	DFF sixth(clk, in[5], out[5]);
-	DFF fifth(clk, in[4], out[4]);
-	DFF fourth(clk, in[3], out[3]);
-	DFF third(clk, in[2], out[2]);
-	DFF second(clk, in[1], out[1]);
-	DFF first(clk, in[0], out[0]);
-	
-endmodule
-
-module Counter();
-endmodule
-
-
-module Velocity();
 endmodule
